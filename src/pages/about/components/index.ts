364e17779f963b2a0d66689e56dd6fe8224d6e9c
parkjihyoun <jihyoun2002@knu.ac.kr>
2025-09-23 01:40:56 +0900

    feat: About 페이지 구조 완전 분리 및 정리

--- src/pages/about/components/index.ts
+++ src/pages/about/components/index.ts
@@ -2,6 +2,8 @@ export { default as AboutHero } from './AboutHero';
 export { default as TextSection } from './TextSection';
 export { default as ArrowImage } from './ArrowImage';
 export { default as ContentSection } from './ContentSection';
+export { default as Professors } from './Professors';
+export { default as GraduationCommittee } from './GraduationCommittee';
 export { default as GraduationMembers } from './GraduationMembers';
 export { default as TeamPhotos } from './TeamPhotos';
 export { default as ScrollArrow } from './ScrollArrow';
