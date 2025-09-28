47aee9bcbda6104bef2f51fa2bc39c342e153deb
Jeong In Jun <jijun897@naver.com>
2025-09-29 00:07:48 +0900

    feat: work 디테일 페이지 구현

--- src/pages/work/WorkDetailPage.tsx
+++ src/pages/work/WorkDetailPage.tsx
@@ -1,7 +1,13 @@
+import { PropLargeSection } from './PropLargeSection';
+import { PropSmallSection } from './PropSmallSection';
+
 const WorkDetailPage = () => {
   return (
-    <div>
-      <h1>Work Detail Page</h1>
+    <div className="pt-[104px] sm:pt-[108px] md:pt-[124px] lg:pt-[170px] bg-black min-h-screen">
+      <img src="/work_image.png" alt="Work Detail" className="w-full h-auto" />
+      <PropSmallSection />
+      <PropLargeSection />
+      <img src="/work_person.png" alt="Work Person" className="w-full h-auto" />
     </div>
   );
 };
--- src/pages/work/index.ts
+++ src/pages/work/index.ts
@@ -1,4 +1,6 @@
 export * from './DetailModal';
 export * from './ArrowButton';
+export * from './PropSmallSection';
+export * from './PropLargeSection';
 export { default as WorkPage } from './WorkPage';
 export { default as WorkDetailPage } from './WorkDetailPage';
