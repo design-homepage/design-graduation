e9c3b4c9178c2842813167dca21715a30b64a35f
Jeong In Jun <jijun897@naver.com>
2025-10-05 02:03:54 +0900

    feat: card 컴포넌트 구현하여 hover 액션 적용

---
+++ src/pages/profile/ProfileCard.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import HoverImage from '/profile/card-hover.jpg';
+
+type ProfileCardProps = {
+  imageUrl: string;
+  name: string;
+};
+
+export const ProfileCard = ({ imageUrl, name }: ProfileCardProps) => {
+  const [isHover, setIsHover] = useState(false);
+
+  return (
+    <div
+      className="relative h-full overflow-hidden"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`w-full h-full object-contain object-center transition-opacity duration-300 ease-out ${isHover ? 'opacity-0' : 'opacity-100'} z-10`}
+        draggable={false}
+      />
+      <img
+        src={HoverImage}
+        alt={name}
+        className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-300 ease-out ${isHover ? 'opacity-100' : 'opacity-0'} z-20`}
+        draggable={false}
+      />
+      <div
+        className={`absolute inset-0 bg-primary/50 flex justify-center items-center transition-opacity duration-300 ease-out z-30 ${isHover ? 'opacity-100' : 'opacity-0'}`}
+      >
+        <p className="text-white text-4xl font-bold">{name}</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,11 +1,12 @@
 import { profile } from './constants/profile';
+import { ProfileCard } from './ProfileCard';
 
 const ProfilePage = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-[10px] sm:gap-[20px] xl:gap-[35px] px-[10px] sm:px-[20px] md:px-[50px] xl:px-[100px]">
       {profile.map((item) => (
         <div key={item.id} className="flex flex-col items-center">
-          <img src={item.profileUrl} alt={item.name} className="w-full object-cover" />
+          <ProfileCard imageUrl={item.profileUrl} name={item.name} />
         </div>
       ))}
     </div>
