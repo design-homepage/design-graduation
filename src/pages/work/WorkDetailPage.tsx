7ce7904524d6db5df0556ef62181d7bd24ac3568
Jeong In Jun <jijun897@naver.com>
2025-10-09 07:59:08 +0900

    feat: work 디테일 페이지 학생 데이터 적용

--- src/pages/work/WorkDetailPage.tsx
+++ src/pages/work/WorkDetailPage.tsx
@@ -1,12 +1,23 @@
+import { useParams } from 'react-router-dom';
 import { PropLargeSection } from './PropLargeSection';
 import { PropSmallSection } from './PropSmallSection';
+import { useMemo } from 'react';
+import { arrows } from './constants/arrows';
 
 const WorkDetailPage = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const arrowData = useMemo(() => arrows.find((item) => item.id.toString() === id), [id]);
+
+  if (!arrowData) {
+    return <div>Profile not found</div>;
+  }
+
   return (
     <>
       <img src="/work/work_image.png" alt="Work Detail" className="w-full h-auto" />
-      <PropSmallSection />
-      <PropLargeSection />
+      <PropSmallSection arrowData={arrowData} />
+      <PropLargeSection arrowData={arrowData} />
       <img src="/work/work_person.png" alt="Work Person" className="w-full h-auto" />
     </>
   );
